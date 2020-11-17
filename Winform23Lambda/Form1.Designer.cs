638057fc071b6cf40571f32d49e8ff81730b3234
kwangbo <giggle_@naver.com>
2020-11-17 13:19:19 +0900

    Lambda

--- Winform23Lambda/Form1.Designer.cs
+++ Winform23Lambda/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnColorChange_1 = new System.Windows.Forms.Button();
+            this.btnColorChange_2 = new System.Windows.Forms.Button();
+            this.btnColorChange_3 = new System.Windows.Forms.Button();
+            this.lboxResult = new System.Windows.Forms.ListBox();
+            this.lblStepCheck = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnColorChange_1
+            // 
+            this.btnColorChange_1.Location = new System.Drawing.Point(40, 25);
+            this.btnColorChange_1.Name = "btnColorChange_1";
+            this.btnColorChange_1.Size = new System.Drawing.Size(166, 23);
+            this.btnColorChange_1.TabIndex = 0;
+            this.btnColorChange_1.Text = "Event  함수를 호출";
+            this.btnColorChange_1.UseVisualStyleBackColor = true;
+            // 
+            // btnColorChange_2
+            // 
+            this.btnColorChange_2.Location = new System.Drawing.Point(40, 71);
+            this.btnColorChange_2.Name = "btnColorChange_2";
+            this.btnColorChange_2.Size = new System.Drawing.Size(166, 23);
+            this.btnColorChange_2.TabIndex = 1;
+            this.btnColorChange_2.Text = "무명 메서드 사용";
+            this.btnColorChange_2.UseVisualStyleBackColor = true;
+            // 
+            // btnColorChange_3
+            // 
+            this.btnColorChange_3.Location = new System.Drawing.Point(40, 127);
+            this.btnColorChange_3.Name = "btnColorChange_3";
+            this.btnColorChange_3.Size = new System.Drawing.Size(166, 23);
+            this.btnColorChange_3.TabIndex = 2;
+            this.btnColorChange_3.Text = "람다식 사용";
+            this.btnColorChange_3.UseVisualStyleBackColor = true;
+            // 
+            // lboxResult
+            // 
+            this.lboxResult.FormattingEnabled = true;
+            this.lboxResult.ItemHeight = 12;
+            this.lboxResult.Location = new System.Drawing.Point(40, 190);
+            this.lboxResult.Name = "lboxResult";
+            this.lboxResult.Size = new System.Drawing.Size(625, 172);
+            this.lboxResult.TabIndex = 3;
+            // 
+            // lblStepCheck
+            // 
+            this.lblStepCheck.AutoSize = true;
+            this.lblStepCheck.Location = new System.Drawing.Point(40, 172);
+            this.lblStepCheck.Name = "lblStepCheck";
+            this.lblStepCheck.Size = new System.Drawing.Size(11, 12);
+            this.lblStepCheck.TabIndex = 4;
+            this.lblStepCheck.Text = "-";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(499, 172);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(166, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "다음";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(804, 374);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblStepCheck);
+            this.Controls.Add(this.lboxResult);
+            this.Controls.Add(this.btnColorChange_3);
+            this.Controls.Add(this.btnColorChange_2);
+            this.Controls.Add(this.btnColorChange_1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnColorChange_1;
+        private System.Windows.Forms.Button btnColorChange_2;
+        private System.Windows.Forms.Button btnColorChange_3;
+        private System.Windows.Forms.ListBox lboxResult;
+        private System.Windows.Forms.Label lblStepCheck;
+        private System.Windows.Forms.Button btnNext;
     }
 }
 
--- Winform23Lambda/Form1.cs
+++ Winform23Lambda/Form1.cs
@@ -16,5 +16,121 @@ namespace Winform23Lambda
         {
             InitializeComponent();
         }
+
+        Action _aStepCheck = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _aStepCheck = () => lblStepCheck.Text = string.Format(" - 다음 Step은  {0}, {1}", iNowStep, ((enumLambdaCase)iNowStep).ToString());
+            _aStepCheck();
+            
+            ButtonColorChange();
+        }
+
+        private void ButtonColorChange()
+        {
+            //event 함수에서 색상변경 
+            btnColorChange_1.Click += BtnColorChange_1_Click;
+
+            //무명메서드 
+            btnColorChange_2.Click += delegate (Object sender, EventArgs e)
+            {
+                btnColorChange_2.BackColor = Color.Brown;
+            };
+
+            //람다식 
+
+            btnColorChange_3.Click += (sender, e) => btnColorChange_3.BackColor = Color.Coral;
+
+        }
+
+        private void BtnColorChange_2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        int iNowStep = 0;
+        delegate int delIntFunc(int a, int b);
+        delegate string delStringFunc();
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            Lambda(iNowStep);
+            iNowStep++;
+            _aStepCheck();
+        }
+
+       
+        private void Lambda(int iCase)
+        {
+            switch (iCase)
+            {
+                case (int)enumLambdaCase.식형식_람다식:
+                    //
+                    delIntFunc dint = (a, b) => a * b;
+                    int iRet = dint(4, 5);
+                    lboxResult.Items.Add(iRet.ToString());
+
+                    delStringFunc dString = () => string.Format("Lambda Sample 식형식");
+                    string sRet = dString();
+                    lboxResult.Items.Add(sRet);
+
+                    break;
+                case (int)enumLambdaCase.문형식_람다식:
+                    // 문형식 람다식
+                    delStringFunc dstrSeqment = () =>
+                    {
+                        return string.Format("Lambda Sample 문형식");
+                    };
+                    string sSeqRet = dstrSeqment();
+                    lboxResult.Items.Add(sSeqRet.ToString());
+
+                    break;
+                case (int)enumLambdaCase.제네릭_형태의_무명메서드_Action:
+                    // action 
+                    Action<string, string> aString = (a, b) =>
+                    {
+                        string strText = String.Format("인자값 {0} 와 {1} 를 받았습니다.", a, b);
+                        lboxResult.Items.Add(strText.ToString());
+                    };
+                    aString("시간", "금");
+                   
+                    break;
+                case (int)enumLambdaCase.제네릭_형태의_무명메서드_Func:
+                    // FUNC : 반환값이 있는 형태 
+                    Func<int, int, int> fint = (a, b) => a * b;
+                    int fintRet = fint(4, 5);
+                    lboxResult.Items.Add(fintRet.ToString());
+                    break;
+                case (int)enumLambdaCase.제네릭_형태의인자_반환_예제:
+                    int[] iGroup = {1,3,5,7,9 };
+                    int iNumSum = iGroup.Sum(x => x );
+                    lboxResult.Items.Add(iNumSum.ToString());
+
+                    string[] strGroup = { "xxx", "TTT", "YYY" };
+                    int iLengthSum = strGroup.Sum(x => x.Length);
+                    lboxResult.Items.Add(iLengthSum.ToString());
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        enum enumLambdaCase
+        {
+             식형식_람다식 = 0,
+             문형식_람다식 = 1,
+             제네릭_형태의_무명메서드_Func =2,
+             제네릭_형태의_무명메서드_Action = 3,
+             제네릭_형태의인자_반환_예제 = 4,
+        }
+
+        private void BtnColorChange_1_Click(object sender, EventArgs e)
+        {
+            btnColorChange_1.BackColor = Color.Aqua;
+        }
+
     }
 }
