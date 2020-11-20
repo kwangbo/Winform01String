86b1a14ba9e8dfd4dcf23bb29bbd5ef1181b7926
kwangbo <giggle_@naver.com>
2020-11-20 14:11:31 +0900

    copy * pater 저장기능 추가

--- Winform32CopyAndPaste/Form1.Designer.cs
+++ Winform32CopyAndPaste/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.lboxTextSave = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -37,10 +38,27 @@
             this.tBar = new System.Windows.Forms.TrackBar();
             this.btnBtextadd = new System.Windows.Forms.Button();
             this.tboxAdd = new System.Windows.Forms.TextBox();
+            this.msMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.파ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장하기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.편집ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.공백제ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.모두제거ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.프로그램정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmComtextMenuScrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.공백제거ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.클립보드ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.모두저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.프로그램정보ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장하기ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.불러오기ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tBar)).BeginInit();
+            this.msMenuStrip.SuspendLayout();
+            this.cmComtextMenuScrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -51,23 +69,24 @@
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 57F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(527, 400);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(527, 376);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // lboxTextSave
             // 
+            this.lboxTextSave.ContextMenuStrip = this.cmComtextMenuScrip;
             this.lboxTextSave.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lboxTextSave.FormattingEnabled = true;
             this.lboxTextSave.ItemHeight = 12;
             this.lboxTextSave.Location = new System.Drawing.Point(3, 43);
             this.lboxTextSave.Name = "lboxTextSave";
-            this.lboxTextSave.Size = new System.Drawing.Size(521, 297);
+            this.lboxTextSave.Size = new System.Drawing.Size(521, 273);
             this.lboxTextSave.TabIndex = 0;
             this.lboxTextSave.SelectedIndexChanged += new System.EventHandler(this.LboxTestSave_SelectedIndexChanged);
             this.lboxTextSave.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lboxTextSave_KeyDown);
@@ -110,7 +129,7 @@
             this.panel2.Controls.Add(this.btnBtextadd);
             this.panel2.Controls.Add(this.tboxAdd);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(3, 346);
+            this.panel2.Location = new System.Drawing.Point(3, 322);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(521, 51);
             this.panel2.TabIndex = 2;
@@ -127,9 +146,9 @@
             // 
             // btnBtextadd
             // 
-            this.btnBtextadd.Location = new System.Drawing.Point(256, 8);
+            this.btnBtextadd.Location = new System.Drawing.Point(256, 16);
             this.btnBtextadd.Name = "btnBtextadd";
-            this.btnBtextadd.Size = new System.Drawing.Size(127, 23);
+            this.btnBtextadd.Size = new System.Drawing.Size(127, 26);
             this.btnBtextadd.TabIndex = 1;
             this.btnBtextadd.Text = "등록";
             this.btnBtextadd.UseVisualStyleBackColor = true;
@@ -137,11 +156,130 @@
             // 
             // tboxAdd
             // 
-            this.tboxAdd.Location = new System.Drawing.Point(10, 10);
+            this.tboxAdd.Location = new System.Drawing.Point(10, 16);
             this.tboxAdd.Name = "tboxAdd";
             this.tboxAdd.Size = new System.Drawing.Size(240, 21);
             this.tboxAdd.TabIndex = 0;
             this.tboxAdd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tboxAdd_KeyDown);
+            // 
+            // msMenuStrip
+            // 
+            this.msMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파ToolStripMenuItem,
+            this.편집ToolStripMenuItem,
+            this.프로그램정보ToolStripMenuItem});
+            this.msMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.msMenuStrip.Name = "msMenuStrip";
+            this.msMenuStrip.Size = new System.Drawing.Size(527, 24);
+            this.msMenuStrip.TabIndex = 1;
+            this.msMenuStrip.Text = "menuStrip1";
+            // 
+            // 파ToolStripMenuItem
+            // 
+            this.파ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.저장하기ToolStripMenuItem,
+            this.불러오기ToolStripMenuItem});
+            this.파ToolStripMenuItem.Name = "파ToolStripMenuItem";
+            this.파ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파ToolStripMenuItem.Text = "파일";
+            // 
+            // 저장하기ToolStripMenuItem
+            // 
+            this.저장하기ToolStripMenuItem.Name = "저장하기ToolStripMenuItem";
+            this.저장하기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.저장하기ToolStripMenuItem.Text = "저장하기";
+            this.저장하기ToolStripMenuItem.Click += new System.EventHandler(this.저장하기ToolStripMenuItem_Click);
+            // 
+            // 불러오기ToolStripMenuItem
+            // 
+            this.불러오기ToolStripMenuItem.Name = "불러오기ToolStripMenuItem";
+            this.불러오기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.불러오기ToolStripMenuItem.Text = "불러오기";
+            this.불러오기ToolStripMenuItem.Click += new System.EventHandler(this.불러오기ToolStripMenuItem_Click);
+            // 
+            // 편집ToolStripMenuItem
+            // 
+            this.편집ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.공백제ToolStripMenuItem,
+            this.모두제거ToolStripMenuItem});
+            this.편집ToolStripMenuItem.Name = "편집ToolStripMenuItem";
+            this.편집ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.편집ToolStripMenuItem.Text = "편집";
+            // 
+            // 공백제ToolStripMenuItem
+            // 
+            this.공백제ToolStripMenuItem.Name = "공백제ToolStripMenuItem";
+            this.공백제ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.공백제ToolStripMenuItem.Text = "공백제거";
+            this.공백제ToolStripMenuItem.Click += new System.EventHandler(this.공백제ToolStripMenuItem_Click);
+            // 
+            // 모두제거ToolStripMenuItem
+            // 
+            this.모두제거ToolStripMenuItem.Name = "모두제거ToolStripMenuItem";
+            this.모두제거ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.모두제거ToolStripMenuItem.Text = "모두제거";
+            this.모두제거ToolStripMenuItem.Click += new System.EventHandler(this.모두제거ToolStripMenuItem_Click);
+            // 
+            // 프로그램정보ToolStripMenuItem
+            // 
+            this.프로그램정보ToolStripMenuItem.Name = "프로그램정보ToolStripMenuItem";
+            this.프로그램정보ToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.프로그램정보ToolStripMenuItem.Text = "프로그램정보";
+            this.프로그램정보ToolStripMenuItem.Click += new System.EventHandler(this.프로그램정보ToolStripMenuItem_Click);
+            // 
+            // cmComtextMenuScrip
+            // 
+            this.cmComtextMenuScrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.공백제거ToolStripMenuItem,
+            this.클립보드ToolStripMenuItem,
+            this.모두저장ToolStripMenuItem,
+            this.프로그램정보ToolStripMenuItem1});
+            this.cmComtextMenuScrip.Name = "cmComtextMenuScrip";
+            this.cmComtextMenuScrip.Size = new System.Drawing.Size(181, 114);
+            // 
+            // 공백제거ToolStripMenuItem
+            // 
+            this.공백제거ToolStripMenuItem.Name = "공백제거ToolStripMenuItem";
+            this.공백제거ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.공백제거ToolStripMenuItem.Text = "공백제거";
+            this.공백제거ToolStripMenuItem.Click += new System.EventHandler(this.공백제거ToolStripMenuItem_Click);
+            // 
+            // 클립보드ToolStripMenuItem
+            // 
+            this.클립보드ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.저장하기ToolStripMenuItem1,
+            this.불러오기ToolStripMenuItem1});
+            this.클립보드ToolStripMenuItem.Name = "클립보드ToolStripMenuItem";
+            this.클립보드ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.클립보드ToolStripMenuItem.Text = "클립보드";
+            // 
+            // 모두저장ToolStripMenuItem
+            // 
+            this.모두저장ToolStripMenuItem.Name = "모두저장ToolStripMenuItem";
+            this.모두저장ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.모두저장ToolStripMenuItem.Text = "모두저장";
+            this.모두저장ToolStripMenuItem.Click += new System.EventHandler(this.모두저장ToolStripMenuItem_Click);
+            // 
+            // 프로그램정보ToolStripMenuItem1
+            // 
+            this.프로그램정보ToolStripMenuItem1.Name = "프로그램정보ToolStripMenuItem1";
+            this.프로그램정보ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.프로그램정보ToolStripMenuItem1.Text = "프로그램정보";
+            this.프로그램정보ToolStripMenuItem1.Click += new System.EventHandler(this.프로그램정보ToolStripMenuItem1_Click);
+            // 
+            // 저장하기ToolStripMenuItem1
+            // 
+            this.저장하기ToolStripMenuItem1.Name = "저장하기ToolStripMenuItem1";
+            this.저장하기ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.저장하기ToolStripMenuItem1.Text = "저장하기";
+            this.저장하기ToolStripMenuItem1.Click += new System.EventHandler(this.저장하기ToolStripMenuItem1_Click);
+            // 
+            // 불러오기ToolStripMenuItem1
+            // 
+            this.불러오기ToolStripMenuItem1.Name = "불러오기ToolStripMenuItem1";
+            this.불러오기ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.불러오기ToolStripMenuItem1.Text = "불러오기";
+            this.불러오기ToolStripMenuItem1.Click += new System.EventHandler(this.불러오기ToolStripMenuItem1_Click);
             // 
             // Form1
             // 
@@ -149,6 +287,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(527, 400);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.msMenuStrip);
+            this.MainMenuStrip = this.msMenuStrip;
             this.Name = "Form1";
             this.Opacity = 0.98D;
             this.Text = "Copy & Paste";
@@ -160,7 +300,11 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tBar)).EndInit();
+            this.msMenuStrip.ResumeLayout(false);
+            this.msMenuStrip.PerformLayout();
+            this.cmComtextMenuScrip.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -175,6 +319,21 @@
         private System.Windows.Forms.TrackBar tBar;
         private System.Windows.Forms.Button btnBtextadd;
         private System.Windows.Forms.TextBox tboxAdd;
+        private System.Windows.Forms.MenuStrip msMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem 파ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장하기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 불러오기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 편집ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 공백제ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 모두제거ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 프로그램정보ToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip cmComtextMenuScrip;
+        private System.Windows.Forms.ToolStripMenuItem 공백제거ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 클립보드ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장하기ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 불러오기ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 모두저장ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 프로그램정보ToolStripMenuItem1;
     }
 }
 
--- Winform32CopyAndPaste/Form1.cs
+++ Winform32CopyAndPaste/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -29,7 +30,7 @@ namespace Winform32CopyAndPaste
 
             //            this.Text = "Title " + oVersion + " " + dd ;
             this.Text = string.Format("{0} Ver.{1}.{2} /Build Time ({3} - {4}) ",
-                "문자복사", oVersion.Major, oVersion.Minor, GetBuildDataTime(oVersion), "상태");
+                "Copy & Paste", oVersion.Major, oVersion.Minor, GetBuildDataTime(oVersion), "상태");
 
             _kbdHook.hook();
             _kbdHook.KeyDown += _kbdHook_KeyDown;
@@ -84,7 +85,7 @@ namespace Winform32CopyAndPaste
 
         }
 
-        
+
 
         private void tBar_Scroll_Change(object sender, EventArgs e)
         {
@@ -142,12 +143,179 @@ namespace Winform32CopyAndPaste
         {
             if (e.KeyCode == Keys.Delete)
             {
-                if (lboxTextSave.SelectedItems.Count > 0 )
+                if (lboxTextSave.SelectedItems.Count > 0)
                 {
                     lboxTextSave.Items.RemoveAt(lboxTextSave.SelectedIndex);
                 }
-                
+
             }
         }
+
+        #region Menu Strip
+        private void 저장하기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fFileSave();
+        }
+        private void 불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fAllLoad();
+        }
+
+        private void 공백제ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fEmptyDelete();
+        }
+
+        private void 모두제거ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fAllDelete();
+        }
+
+        private void 프로그램정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fProgramInfo();
+        }
+
+        #endregion
+        #region Context Menu Strip
+
+        private void 저장하기ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            fFileSave();
+        }
+
+        private void 불러오기ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            fAllLoad();
+        }
+
+        private void 공백제거ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fEmptyDelete();
+        }
+
+        private void 모두저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fFileSave();
+        }
+
+        private void 프로그램정보ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            fProgramInfo();
+        }
+
+
+        #endregion
+        /// <summary>
+        /// 공백제거
+        /// </summary>
+        #region Event Function
+
+        private void fEmptyDelete()
+        {
+            try
+            {
+                int iCount = lboxTextSave.Items.Count;
+                for (int i = 0; i < iCount; i++)
+                {
+                    lboxTextSave.Items[i] = lboxTextSave.Items[i].ToString().Trim();
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        /// <summary>
+        /// 모두삭제 
+        /// </summary>
+        private void fAllDelete()
+        {
+            if (DialogResult.Yes == MessageBox.Show("등록되어 있는 Data를 초기화 합니다.",
+                "ListBox Item Cleare", MessageBoxButtons.YesNo))
+            {
+                lboxTextSave.Items.Clear();
+            }
+        }
+
+        /// <summary>
+        /// 저장하기용 함수 
+        /// </summary>
+        private void fFileSave()
+        {
+            SaveFileDialog SFDialog = new SaveFileDialog();
+
+            int ilbCount = lboxTextSave.Items.Count;
+            string strFilePath = string.Empty;
+
+            SFDialog.InitialDirectory = Application.StartupPath;
+            SFDialog.FileName = "*.txt";
+            SFDialog.Filter = "txt files(*.txt) | *.txt | All Files (*.*) | *.*";
+
+            try
+            {
+                if (SFDialog.ShowDialog() == DialogResult.OK)
+                {
+                    strFilePath = SFDialog.FileName;
+                    StreamWriter swSFDialog = new StreamWriter(strFilePath);
+
+                    for (int i = 0; i < ilbCount; i++)
+                    {
+                        swSFDialog.WriteLine(lboxTextSave.Items[i].ToString());
+                    }
+                    swSFDialog.Close();
+
+                    MessageBox.Show("저장이 완료 되었습니다.");
+
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// 불러오기용 함수 
+        /// </summary>
+        private void fAllLoad()
+        {
+            OpenFileDialog OFDialog = new OpenFileDialog();
+            string strFilePath = string.Empty;
+
+            OFDialog.InitialDirectory = Application.StartupPath;
+            OFDialog.FileName = "*.txt";
+            OFDialog.Filter = "txt files (*.txt)|*.txt|All Files(*.*)|*.*";
+            try
+            {
+                if (OFDialog.ShowDialog() == DialogResult.OK)
+                {
+                    strFilePath = OFDialog.FileName;
+                    StreamReader srOFDialoog = new StreamReader(strFilePath,
+                                                                Encoding.UTF8,
+                                                                true);
+                    while (srOFDialoog.EndOfStream == false)
+                    {
+                        lboxTextSave.Items.Add(srOFDialoog.ReadLine());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        /// <summary>
+        /// 프로그램 정보 함수 
+        /// </summary>
+        private void fProgramInfo()
+        {
+            string strPrograminfo = "반복작업을 조금이나마 수월하게!!!";
+            MessageBox.Show(strPrograminfo);
+        }
+
+
+        #endregion
     }
 }
